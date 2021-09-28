d3c43ba9393cb3be5d2dcac5f567cdfa56ea54cd
GoatedChopin <85979611+GoatedChopin@users.noreply.github.com>
2021-09-27 23:20:30 -0600

    Add files via upload

---
+++ ItalianWedding/src/CSVReader.java
@@ -0,0 +1,4 @@
+public class CSVReader {
+    public String poop = "Solid Farts";
+    // todo - make this read the csv's into their respective Person[] arrays.
+}
---
+++ ItalianWedding/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // read in sigs and phis
+        CSVReader csvReader = new CSVReader();
+
+        // build the two arrays with this shit? idk
+
+        Person[] sigs = new Person[0];
+        Person[] phis = new Person[0];
+
+        GuestMap guestMap = new GuestMap(sigs, phis);
+
+        // use the guestMap to create a label text document
+
+    }
+}
